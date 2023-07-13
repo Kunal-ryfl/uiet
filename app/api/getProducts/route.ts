943874e89cd95f2fe0b26a88a1831357ec078a64
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-07-13 11:23:16 +0530

    APIs example

---
+++ app/api/getProducts/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server"
+type item = {
+    name:string
+    price:number,
+    rating:number
+}
+
+let products:item[] = [
+    {name:'redmi',price:100,rating:3},
+    {name:'oneplus',price:300,rating:5},
+    {name:'samsung',price:200,rating:1},
+]
+
+export async function GET() {
+    return NextResponse.json({products:products})
+  }
+  
